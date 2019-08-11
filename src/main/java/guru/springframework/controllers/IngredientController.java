3d6be86621b51e05148bb3a4624f1f2676f13bb0
Jopkirs <megaalexs@yahoo.com>
2019-08-11 17:22:55 -0400

    Add show an ingredient functionality.

--- src/main/java/guru/springframework/controllers/IngredientController.java
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -1,5 +1,6 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.IngredientService;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -12,17 +13,30 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IngredientController {
 
     private final RecipeService recipeService;
+    private final IngredientService ingredientService;
 
-    public IngredientController(RecipeService recipeService) {
+    public IngredientController(RecipeService recipeService, IngredientService ingredientService) {
         this.recipeService = recipeService;
+        this.ingredientService = ingredientService;
     }
 
     @GetMapping("/recipe/{id}/ingredients")
-    public String listIngredients(@PathVariable String id, Model model){
+    public String listIngredients(@PathVariable String id, Model model) {
         log.debug("Getting ingredients for recipe id: " + id);
 
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
 
         return "recipe/ingredient/list";
+    }
+
+    @GetMapping("recipe/{recipeId}/ingredient/{id}/show")
+    public String showRecipeIngredient(@PathVariable String recipeId,
+                                       @PathVariable String id, Model model) {
+
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(
+                Long.valueOf(recipeId),
+                Long.valueOf(id)));
+
+        return "recipe/ingredient/show";
     }
 }

c943d102a46d2af56542bd862ad9e8a75089d66e
Jopkirs <megaalexs@yahoo.com>
2019-08-12 11:52:19 -0400

    Make image show after uploading it to the Database.

--- src/main/java/guru/springframework/converters/RecipeToRecipeCommand.java
+++ src/main/java/guru/springframework/converters/RecipeToRecipeCommand.java
@@ -40,7 +40,7 @@ public class RecipeToRecipeCommand implements Converter<Recipe, RecipeCommand> {
         command.setServings(recipe.getServings());
         command.setSource(recipe.getSource());
         command.setUrl(recipe.getUrl());
-
+        command.setImage(recipe.getImage());
         command.setNotes(notesToNotesCommand.convert(recipe.getNotes()));
 
         if(recipe.getCategories() != null && recipe.getCategories().size() > 0){
--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -4,6 +4,7 @@ import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -20,6 +21,7 @@ public class ImageServiceImpl implements ImageService{
     }
 
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile image) {
         try {
             Recipe recipe = recipeRepository.findById(recipeId).get();
